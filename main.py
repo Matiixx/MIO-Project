e1f1edfa1d6ba2dc5b853f32d299bc5d45421a5e
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-17 18:25:54 +0200

    create main

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from model import create_custom_model
+from model import create_custom_model, predict_populatiry
+from utils import reduce_dataset
+import sys
 
-create_custom_model('data/realdonaldtrump.csv')
+
+def main(argv):
+    if (argv == "create_model"):
+        create_custom_model('data/reduced_data.csv')
+    elif (argv == "reduce_dataset"):
+        reduce_dataset('data/realdonaldtrump.csv')
+    elif (argv == "predict_popularity"):
+        print(predict_populatiry(
+            "\"My persona will never be that of a wallflower - I’d rather build walls than cling to them\" --Donald J. Trump"))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
