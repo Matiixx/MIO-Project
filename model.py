d62265f672778180d148b36c701c9cbac11cbae2
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-17 18:24:58 +0200

    save model

--- model.py
+++ model.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+import pickle
 
 
 def create_custom_model(data_path):
@@ -15,7 +17,7 @@ def create_custom_model(data_path):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=321)
 
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(min_df=10)
     X_train_vectorized = vectorizer.fit_transform(X_train).toarray()
     X_test_vectorized = vectorizer.transform(X_test).toarray()
 
@@ -30,6 +32,15 @@ def create_custom_model(data_path):
 
     mse = mean_squared_error(y_test, y_pred)
     print("Mean Squared Error:", mse)
+
+    mae = mean_absolute_error(y_test, y_pred)
+    print("Mean Absolute Error:", mae)
+
+    filename = 'finalized_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+    filename = 'vectorizer.sav'
+    pickle.dump(vectorizer, open(filename, 'wb'))
 
 
 def predict_populatiry(post):
