b348ab67c63464a5704c561149fec13c79f11df6
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-18 16:05:24 +0200

    update model

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import xgboost as xgb
 import pickle
 
 
@@ -18,7 +17,7 @@ def create_custom_model(data_path):
         X, y, test_size=0.2, random_state=321
     )
 
-    vectorizer = TfidfVectorizer(min_df=10)
+    vectorizer = TfidfVectorizer(min_df=20)
     X_train_vectorized = vectorizer.fit_transform(X_train).toarray()
     X_test_vectorized = vectorizer.transform(X_test).toarray()
 
@@ -26,10 +25,17 @@ def create_custom_model(data_path):
     y_train = np.array([label_to_index[label] for label in y_train])
     y_test = np.array([label_to_index[label] for label in y_test])
 
-    model = LinearRegression()
+    model = xgb.XGBRegressor()
     model.fit(X_train_vectorized, y_train)
 
     y_pred = model.predict(X_test_vectorized)
+    y_train_pred = model.predict(X_train_vectorized)
+
+    mse = mean_squared_error(y_train, y_train_pred)
+    print("Mean Squared Error:", mse)
+
+    mae = mean_absolute_error(y_train, y_train_pred)
+    print("Mean Absolute Error:", mae)
 
     mse = mean_squared_error(y_test, y_pred)
     print("Mean Squared Error:", mse)
@@ -37,17 +43,17 @@ def create_custom_model(data_path):
     mae = mean_absolute_error(y_test, y_pred)
     print("Mean Absolute Error:", mae)
 
-    filename = "finalized_model.sav"
+    filename = "finalized_model.pkl"
     pickle.dump(model, open(filename, "wb"))
 
-    filename = "vectorizer.sav"
+    filename = "vectorizer.pkl"
     pickle.dump(vectorizer, open(filename, "wb"))
 
 
 def predict_populatiry(content):
-    filename = "finalized_model.sav"
+    filename = "finalized_model.pkl"
     model = pickle.load(open(filename, "rb"))
-    filename = "vectorizer.sav"
+    filename = "vectorizer.pkl"
     vectorizer = pickle.load(open(filename, "rb"))
     content_vectorized = vectorizer.transform([content]).toarray()
     return model.predict(content_vectorized)[0]
