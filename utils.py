e78d2161279a421dd0e6f40a9db5faeffa49c241
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-18 16:05:34 +0200

    use all data set

--- utils.py
+++ utils.py
@@ -6,5 +6,4 @@ def reduce_dataset(path):
     data = pd.read_csv(f"{path}")
     data = data[["content", "favorites"]]
     data = shuffle(data)
-    data = data[1:5000]
     data.to_csv("data/reduced_data.csv", index=False)
